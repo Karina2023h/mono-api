b0d7c39830f8f762ea25226662a0c9471da7fddd
Karina2023h <127620416+Karina2023h@users.noreply.github.com>
2026-06-01 18:32:08 +0200

    Change response format to plain text
    Refactor handler to return raw text response instead of JSON.

--- api/mono.js
+++ api/mono.js
@@ -1,52 +1,16 @@
 export default async function handler(req, res) {
 
-  try {
-
-    const response = await fetch(
-      "https://api.monobank.ua/personal/client-info",
-      {
-        headers: {
-          "X-Token": process.env.MONO_TOKEN
-        }
+  const response = await fetch(
+    "https://api.monobank.ua/personal/client-info",
+    {
+      headers: {
+        "X-Token": process.env.MONO_TOKEN
       }
-    );
-
-    const data = await response.json();
-
-    const jars = data.jars || [];
-
-    const jar = jars.find(
-      item => item.sendId === "jar/AScRAeMaCs"
-    );
-
-    if (!jar) {
-
-      return res.status(404).json({
-        error: "Jar not found",
-        availableJars: jars
-      });
-
     }
+  );
 
-    const collected = Math.round(jar.balance / 100);
-    const goal = Math.round(jar.goal / 100);
+  const text = await response.text();
 
-    return res.status(200).json({
-      title: jar.title,
-      collected,
-      goal,
-      percent: Math.round(
-        collected / goal * 100
-      ),
-      remaining: goal - collected
-    });
-
-  } catch (error) {
-
-    return res.status(500).json({
-      error: error.message
-    });
-
-  }
+  return res.status(200).send(text);
 
 }
